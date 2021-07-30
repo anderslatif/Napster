272525af732902a5a9ffb667d4e3c176d5b8ebdb
Anders Latif <andl@kea.dk>
2021-07-30 12:33:27 +0200

    Created the first untested version of the tabs/tabsview

---
+++ src/GenericComponents/Tabs/Tabs.svelte
@@ -0,0 +1,51 @@
+<script context="module">
+    export const TABS = {};
+</script>
+
+<script>
+    import { setContext, onDestroy } from "svelte";
+    import { writable } from "svelte/store";
+
+    const tabs = [];
+    const views = [];
+    const selectedTab = writiable(null);
+    const selectedView = writable(null);
+
+    setContext(TABS, {
+
+        registerTab: tab => {
+            tabs.push(tab);
+            selectedTab.update(current => current || tab);
+
+            onDestroy(() => {
+                const index = tabs.indexOf(tab);
+                tabs.splice(index, 1);
+                selectedTab.update(current => current === tab ? (tabs[index] || tabs[tabs.length-1]) : current);
+            });
+        },
+
+        registerView: view => {
+            views.push(view);
+            selectedView.update(current => current || view);
+
+            onDestroy(() => {
+                const index = views.indexOf(view);
+                views.splice(index, 1);
+                selectedView.update(current => current === view ? (views[index] || views[views.length -1]) : current);
+            });
+        },
+
+        selectTab: tab => {
+            const index = tabs.indexOf(tab);
+            selectedTab.set(tab);
+            selectedPanel.set(panels[i]);
+        },
+
+        selectedTab,
+        selectedView
+    });
+</script>
+
+<div class="tabs">
+    <slot></slot>
+</div>
---
+++ src/GenericComponents/Tabs/tabs.js
@@ -0,0 +1,4 @@
+export { default as Tabs } from './Tabs.svelte';
+export { default as TabList } from './TabList.svelte';
+export { default as TabView } from './TabView.svelte';
+export { default as Tab } from './Tab.svelte';
