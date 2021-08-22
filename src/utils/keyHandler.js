a88c9e1b5c6ec147c07d52de33d6dcc1189349de
Anders Latif <andl@kea.dk>
2021-08-22 11:35:45 +0200

    Implemented the arrow key up/down (+shift) selection of items

--- src/utils/keyHandler.js
+++ src/utils/keyHandler.js
@@ -5,7 +5,6 @@ import { skipSong, playOrPauseSong } from "../playlist/howler.js";
 const pressedKeys = {};
 
 export function keyDown(event) {
-    event.preventDefault();
     pressedKeys[event.code] = true;
     keyHandler();
 }
@@ -38,6 +37,49 @@ function keyHandler() {
 
     if (pressedKeys.Space) {
         playOrPauseSong();
+    }
+
+    // logic for selecting songs with up and down arrows
+    if (pressedKeys.ShiftLeft || pressedKeys.ShiftRight) {
+        if (pressedKeys.ArrowUp) {
+            const selectedRows = document.querySelectorAll(".selected");
+            const firstId = Number(selectedRows[0]?.getAttribute("data-index"));
+            if (firstId && firstId-1 >= 0) {
+                document.querySelector(`[data-index~="${firstId-1}"]`).classList.add("selected");
+            }
+        } else if (pressedKeys.ArrowDown) {
+            const selectedRows = document.querySelectorAll(".selected");
+            const lastId = Number(selectedRows[selectedRows.length-1]?.getAttribute("data-index"));
+            if (lastId || lastId === 0) {
+                const nextRow = document.querySelector(`[data-index~="${lastId+1}"]`)
+                nextRow?.classList.add("selected");
+            }
+        }
+    } else if (pressedKeys.ArrowUp) {
+        const selectedRows = document.querySelectorAll(".selected");
+        const firstId = Number(selectedRows[0]?.getAttribute("data-index"));
+        
+        if (firstId && firstId-1 >= 0) {
+            document.querySelector(`[data-index~="${firstId-1}"]`).classList.add("selected");
+            selectedRows.forEach(selectedRow => selectedRow.classList.remove("selected"));
+        } else if (firstId <= 0) {
+            selectedRows.forEach(selectedRow => selectedRow.classList.remove("selected"));
+            document.querySelector(`[data-index~="${0}"]`).classList.add("selected");
+        }
+    } else if (pressedKeys.ArrowDown) {
+        const selectedRows = document.querySelectorAll(".selected");
+        const lastId = Number(selectedRows[selectedRows.length-1]?.getAttribute("data-index"));
+
+        const nextRow = document.querySelector(`[data-index~="${lastId+1}"]`)
+
+        selectedRows.forEach(selectedRow => selectedRow.classList.remove("selected"));
+
+        if (!nextRow) {
+            const lastRow = document.querySelector(`[data-index~="${lastId}"]`);
+            lastRow?.classList.add("selected");
+        } else if (lastId || lastId === 0) {
+            nextRow.classList.add("selected");
+        }
     }
 
     if (pressedKeys.KeyD && (pressedKeys.MetaLeft || pressedKeys.MetaRight)) {
